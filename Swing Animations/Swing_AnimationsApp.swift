da1f4c3482eac3c20105fc00c3cee54bb1eb2069
NazarStf <hworker98@gmail.com>
2023-07-29 13:40:12 +0300

    Initial Commit

---
+++ Swing Animations/Swing_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swing_AnimationsApp.swift
+//  Swing Animations
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swing_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
